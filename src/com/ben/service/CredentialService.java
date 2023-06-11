edcc53b950c58e370173a489a041aa9b787bde7f
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-06-11 23:55:02 +0530

    Update CredentialService.java

--- src/com/ben/service/CredentialService.java
+++ src/com/ben/service/CredentialService.java
@@ -3,13 +3,7 @@ package com.ben.service;
 import com.ben.EmailImpl.Employee;
 
 public interface CredentialService {
-
     public  String generatePassword();
     public String generateEmailAddress(Employee employee);
-
     public String showDetail(Employee employee);
-
-
-
-
 }
