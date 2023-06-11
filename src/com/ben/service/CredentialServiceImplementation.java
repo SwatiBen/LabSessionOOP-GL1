c23edec2061d4ab33aa8f5c6e84318e18c1886ac
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-06-11 23:56:27 +0530

    Update CredentialServiceImplementation.java

--- src/com/ben/service/CredentialServiceImplementation.java
+++ src/com/ben/service/CredentialServiceImplementation.java
@@ -4,12 +4,10 @@ import java.util.Random;
 
 import com.ben.EmailImpl.Employee;
 
-public class CredentialServiceImplementation implements CredentialService{
-	
-    public static final int passwordLength = 8;
-    public static final String COMPDOM_STRING = ".ben.com";
-    
-    public String generatePassword() {
+public class CredentialServiceImplementation implements CredentialService {
+	public static final int passwordLength = 8;
+	public static final String COMPDOM_STRING = ".ben.com"; 
+        public String generatePassword() {
 
         String capString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerCase = capString.toLowerCase();
@@ -24,7 +22,6 @@ public class CredentialServiceImplementation implements CredentialService{
         for(int i=0;i<passwordLength;i++){
             filter = filter + finalCase.charAt(random.nextInt(finalCase.length()));
         }
-
         return filter;
     }
 
