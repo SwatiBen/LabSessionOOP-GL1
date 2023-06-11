4f348d980d9213045e9a4532120f871e0e354915
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-06-11 23:04:04 +0530

    Add files via upload

---
+++ LabSessionOOP/src/com/ben/service/CredentialServiceImplementation.java
@@ -0,0 +1,48 @@
+package com.ben.service;
+
+
+
+import java.util.Random;
+
+import com.ben.EmailImpl.Employee;
+
+public class CredentialServiceImplementation implements CredentialService{
+	
+    public static final int passwordLength = 8;
+    public static final String COMPDOM_STRING = ".gl.com";
+    
+    public String generatePassword() {
+
+        String capString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCase = capString.toLowerCase();
+        String num = "0123456789";
+        String splchar = "!@#$%^&*~";
+
+        String finalCase = capString + lowerCase + num + splchar;
+        String pass = "";
+
+        Random random = new Random();
+
+        for(int i=0;i<passwordLength;i++){
+            pass = pass + finalCase.charAt(random.nextInt(finalCase.length()));
+        }
+
+        return pass;
+    }
+
+    public String generateEmailAddress(Employee employee) {
+        String firstName = employee.getFirstName().toLowerCase();
+        String lastName = employee.getLastName().toLowerCase();
+        String department = employee.getDepartment();
+
+        return firstName + lastName + "@"+ department + COMPDOM_STRING;
+    }
+
+    public String showDetail(Employee employee) {
+        System.out.println("Dear "+employee.getFirstName()+" your generated credentials are as follows");
+        System.out.println("Email --> "+employee.getEmail());
+        System.out.println("Password --> "+employee.getPassword());
+
+        return null;
+    }
+}
